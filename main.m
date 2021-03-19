531426f43f612bc1612520f9b6646e47bdab2b86
Alessandro <alessandro.grattarola@iit.it>
2021-03-19 10:39:50 +0100

    Wrapped angle

--- main.m
+++ main.m
@@ -11,17 +11,17 @@ clear
 vmin= 1;
 vmax= 3;
 v=vmin+rand()*(vmax-vmin);
-wmin= -1;
-wmax= 1;
+wmin= -3;
+wmax= 3;
 w=wmin+rand()*(wmax-wmin);
 
 % Create noise and state of the robot
-Q = [ 0.005, 0, 0; 0, 0.005, 0; 0, 0, 0.00005 ];
-N = [ 0.002, 0; 0, 0.0002 ];
-noise = mvnrnd([0,0,0], Q)';
+Q = [ 0.05, 0, 0; 0, 0.05, 0; 0, 0, 0.0005 ];
+N = [ 0.02, 0; 0, 0.002 ];
+noise = mvnrnd([0,0,0], Q.^2)';
 landmark = [10, 30; -10, -26; 78, -45; -68, 14 ];
 % n_landmarks = size(landmark, 1);
-X = [0; 0; 0];
+X = [0; 0; pi/2];
 X_true = X + noise;
 dt = 0.01;
 
@@ -39,12 +39,15 @@ i = 0;
 T = 0;
 % rng('default')
 
-covariance = Q;
-% bearing_array = [];
-% bearing_array_true = [];
+diverge = 0;
 
+covariance = Q;
+bearing_array = [];
+bearing_array_true = [];
+command_array = [];
+model_noise_array = noise;
 %% Loop
-while( T < 30)
+while( T < 30 && diverge == 0)
 
 % Create random input
 v=vmin+rand()*(vmax-vmin);
@@ -52,9 +55,9 @@ w=wmin+rand()*(wmax-wmin);
 u = [v, w];
 
 % Create state and measure noise
-noise = mvnrnd([0,0,0], Q)';
-noise_measure = mvnrnd([0, 0], N)';
-
+noise = mvnrnd([0,0,0], Q.^2)';
+noise_measure = mvnrnd([0, 0], N.^2)';
+model_noise_array = [model_noise_array; noise];
 % Move forward the robot (adding noise) and prediction step
 covariance = predict_covariance(X, covariance, Q, u, dt);
 X_true = move_forward(X_true, u, dt, noise);
@@ -65,11 +68,15 @@ X = prediction_step(X, u, dt);
 [range_true, bearing_true] = measure_step(X_true, landmark, noise_measure);
 [range, bearing] = measure_prediction(X, landmark);
 
+if (abs(bearing(1)-bearing_true(1)) > 0.5 || abs(bearing(2)-bearing(2)) > 0.5 || abs(bearing(3)-bearing_true(3)) > 0.5 || abs(bearing(4)-bearing(4)) > 0.5)
+    diverge = 1;
+    X_prior = X;
+end
 
 update(1) = 0; 
 update(2) = 0;
 
-if mod(i, 10) == 0 && i~=0
+if mod(i, 50) == 0 && i~=0
 [X, covariance] = update_step(X, landmark, range, bearing, range_true, bearing_true, covariance, N);
 update(i,1) = X(1);
 update(i,2) = X(2);
@@ -85,9 +92,9 @@ theta_true = [theta_true, X_true(3)];
 covariance_x = [covariance_x, covariance(1,1)];
 covariance_y = [covariance_y, covariance(2,2)];
 covariance_theta= [covariance_theta, covariance(3,3)];
-% bearing_array_true = [bearing_array_true, bearing_true];
-% bearing_array = [bearing_array, bearing];
-
+bearing_array_true = [bearing_array_true; bearing_true];
+bearing_array = [bearing_array; bearing];
+command_array = [command_array; u];
 i = i+1;
 T = T+dt;
 
@@ -98,11 +105,14 @@ end
 figure  
 hold on 
 plot(trajectory_x_true, trajectory_y_true)
-% plot(landmark(1), landmark(2), 'x')
+% for j=1:4
+% plot(landmark(j,1), landmark(j,2), 'x')
+% end
 plot(trajectory_x, trajectory_y)
 % plot(update(:,1), update(:,2), 'x')
 axis equal
-
+% 
+%% Plot other stuff
 figure  
 subplot(2,1,1)
 plot(trajectory_x)
@@ -114,12 +124,12 @@ subplot(2,1,1)
 plot(trajectory_y)
 subplot(2,1,2)
 plot(trajectory_y_true)
-
-figure  
-subplot(2,1,1)
-plot(theta)
-subplot(2,1,2)
-plot(theta_true)
+% 
+% figure  
+% subplot(2,1,1)
+% plot(theta)
+% subplot(2,1,2)
+% plot(theta_true)
 
 figure  
 subplot(3,1,1)
@@ -129,13 +139,28 @@ plot(covariance_y)
 subplot(3,1,3)
 plot(covariance_theta)
 
+figure  
+subplot(2,1,1)
+plot(bearing_array(:,4))
+subplot(2,1,2)
+plot(bearing_array_true(:,4))
+
+% figure
+% subplot(2,1,1)
+% plot(command_array(:,1))
+% subplot(2,1,2)
+% plot(command_array(:,2))
+
+% figure
+% plot(model_noise_array)
+
+
 %% Plot animated trajectory
 figure('Name', 'Animated');
-ylim([-10 10]);
+ylim([-10 100]);
 xlim([-1 100]);
-axis equal
-hPlot = plot(NaN,NaN,'ro');
-for k=1:size(trajectory_x,2)
+
+for k=2350:size(trajectory_x,2)
      cla
      hold on
 %   set(hPlot, 'XData', trajectory_x(k), 'YData', trajectory_y(k))
@@ -144,7 +169,10 @@ for k=1:size(trajectory_x,2)
     plot(trajectory_x_true(k), trajectory_y_true(k), 'x', 'MarkerSize', 8);
     plot([trajectory_x_true(k), trajectory_x_true(k) + 2*cos(theta_true(k))], [trajectory_y_true(k), trajectory_y_true(k) + 2*sin(theta_true(k))]);
 
-    ylim([-10 10]);
+    ylim([-10 100]);
     xlim([-1 100]);
-    pause(0.05);
+    pause(0.1);
 end
+
+% array = [];
+% noise = mvnrnd([0,0,0], Q)';
--- update_step.m
+++ update_step.m
@@ -3,26 +3,28 @@ function [updated_state, updated_covariance, K] = update_step(X, landmark, range
 sum_1 = 0;
 sum_2 = 0;
 n_landmark = size(landmark, 1);
-
+active_landmark = 0;
 for i = 1:n_landmark
-    
-y = [range_true(i); bearing_true(i)] - [range(i); bearing(i)];
+   if abs(bearing(i) - bearing_true(i)) < 1
+        y = [range_true(i); bearing_true(i)] - [range(i); bearing(i)];
 
-% Jacobian of the measurement model
-H = [(X(1) - landmark(i,1))/range(i), (X(2) - landmark(i,2))/range(i), 0; ...
-    (landmark(i,2) - X(2))/range(i)^2, (X(1) - landmark(i,1))/range(i)^2, -1];
+        % Jacobian of the measurement model
+        H = [(X(1) - landmark(i,1))/range(i), (X(2) - landmark(i,2))/range(i), 0; ...
+            (landmark(i,2) - X(2))/range(i)^2, (X(1) - landmark(i,1))/range(i)^2, -1];
 
-S = H*covariance*H'+ N;
-K = covariance*H'*pinv(S);
+        S = H*covariance*H'+ N;
+        K = covariance*H'*pinv(S);
 
-sum_1 =  sum_1 + K*y;
-sum_2 = sum_2 + K*H;
-
+        sum_1 =  sum_1 + K*y;
+        sum_2 = sum_2 + K*H;
+        active_landmark = active_landmark + 1;
+   end
 end
 
 
-updated_state = X + sum_1/n_landmark;
-updated_covariance = (eye(3) - sum_2/n_landmark)*covariance;
+updated_state = X + sum_1/active_landmark;
+updated_covariance = (eye(3) - sum_2/active_landmark)*covariance;
+
 
 end
 
